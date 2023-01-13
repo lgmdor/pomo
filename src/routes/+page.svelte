c97dbeb35189d949a0cf862537b3b968d6a69db5
lgmdor <lgm.dor@gmail.com>
2023-01-13 21:17:23 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<!--------markup-------->
+
+<div class="wrap" />
+
+<!--------style-------->
+<style lang="sass">
+  @use 'src/vars'
+  @use 'src/oc'
+
+
+  .wrap
+    border: vars.$border
+    width: vars.$size-20
+    height: vars.$size-24
+</style>
