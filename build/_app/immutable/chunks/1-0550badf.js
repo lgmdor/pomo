8ae7cf46d994569fefc24d87932b0262cfecc394
lgmdor <lgm.dor@gmail.com>
2023-02-03 23:14:58 +0100

    deleted build

--- build/_app/immutable/chunks/1-0550badf.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-e49caa33.js";export{t as component};
--- build/_app/immutable/chunks/2-b81cf70c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-525f77b2.js";export{t as component};
